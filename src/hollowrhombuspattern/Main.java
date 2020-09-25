59f84f65b3e980432575cceea706224409c334d3
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-09-25 17:47:07 +0530

    Added some code

---
+++ src/hollowrhombuspattern/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @author Pranshu Aggarwal
+ * @problem 
+ */
+package hollowrhombuspattern;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(sc.readLine());
+		for (int row = 1, start = n, end = 2 * n - 1; row <= n; row++, start--, end--) {
+			for (int col = 1; col <= end; col++) if (col >= start && ((row == 1 || row == n) || (col == start || col == end))) System.out.print("*"); else System.out.print(" ");
+			System.out.println();
+		}
+	}
+}
