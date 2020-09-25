78d5e2cbd1d6cf2730ef3bd1d942769d79b6b110
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-09-25 17:02:55 +0530

    Added some code

---
+++ src/prateeklovescandy/main.py
@@ -0,0 +1,20 @@
+'''
+ @author: Pranshu Aggarwal
+ @problem: https://hack.codingblocks.com/app/practice/1/67/problem
+'''
+MAX_SIZE = 1000005
+primes = [0,2]
+def sieve_of_eratosthenes():
+    temp = [True if x%2 else False for x in range(0,MAX_SIZE)]
+    temp[1], temp[2] = 0, 1
+    for i in range(3, MAX_SIZE,2): 
+        if(i*i<MAX_SIZE and temp[i] is True): 
+            for j in range(i*i, MAX_SIZE,i): temp[j] = False
+    for i in range(3, MAX_SIZE,2): 
+        if temp[i] is True: primes.append(i)
+
+sieve_of_eratosthenes()
+testcases = int(input())
+for testcase in range(testcases):
+    n = int(input())
+    print(primes[n])
