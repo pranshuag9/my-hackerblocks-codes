1669910a583956664059dc2c2c45834fd2da1d37
kanag3 <72195506+kanag3@users.noreply.github.com>
2020-10-01 15:18:55 +0530

    Optimized the code
    Changed the Input Reader from BufferedReader to Scanner Class to handle the input better by the user.

--- src/von_neuman_loves_binary/Main.java
+++ src/von_neuman_loves_binary/Main.java
@@ -3,8 +3,7 @@
  * @problem https://hack.codingblocks.com/app/practice/1/219/problem
  */
 package von_neuman_loves_binary;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 public class Main {
     static int myPow(int b,int power){
         int result=1;
@@ -31,10 +30,10 @@ public class Main {
         return decimal;
     }
     public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        int testcases = Integer.parseInt(sc.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         for(int testcase = 0; testcase < testcases; testcase++){
-        	int binary = Integer.parseInt(sc.readLine());
+        	int binary = sc.nextInt();
             int decimal = binary_to_decimal(binary);
             System.out.println(decimal);
         }
