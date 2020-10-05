e5f94fff0683949598e5d4ae724352d7a36c5ef7
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-10-06 00:01:14 +0530

    Added Odd Even Back in Delhi Code

--- src/odd_and_even_back_in_delhi/Main.java
+++ src/odd_and_even_back_in_delhi/Main.java
@@ -7,9 +7,6 @@ package odd_and_even_back_in_delhi;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Main {
 	static boolean odd_and_Even_back_in_delhi(int n) {
 		int even = 0, odd = 0;
