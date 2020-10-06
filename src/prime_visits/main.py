17f072c2a4a9c9fabe620a62f685b816bde367a9
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-10-07 00:13:33 +0530

    Added Prime Visits Solution

--- src/prime_visits/main.py
+++ src/prime_visits/main.py
@@ -2,13 +2,16 @@
  @author: Pranshu Aggarwal
  @problem: https://hack.codingblocks.com/app/practice/1/1039/problem
 '''
+"""
+TODO: OPTIMIZE THE CODE FOR TIME LIMIT EXCEEDED ERROR
+"""
 MAX_SIZE = 1000005
 primes = [True if i%2==1 else False for i in range(MAX_SIZE)]
 primes[1], primes[2] = 0, 1
 def sieve_of_eratosthenes():
     for i in range(3, len(primes), 2): primes[i] = True
     for i in range(3, len(primes), 2):
-        if (i * i < len(primes)) or (primes[i] is True):
+        if (i * i < len(primes)) or (primes[i] == True):
             for j in range(i * i, len(primes), i): primes[j] = False 
 def prime_visits(a, b):
     result = 0
