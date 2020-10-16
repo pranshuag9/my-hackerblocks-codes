b223e64d75aacf6d81a6d323cbd5d05abcfa6ce6
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-10-16 22:33:00 +0530

    Added GCD solution code

--- src/gcd/Main.java
+++ src/gcd/Main.java
@@ -11,17 +11,31 @@ public class Main {
 	static BigInteger gcd(BigInteger n1, BigInteger n2) {
 		return n1.gcd(n2);
 	}
-	
+	static long gcdI(long n1, long n2) {
+		while(n1%n2!=0) {
+			long rem = n1%n2;
+			n1 = n2;
+			n2 = rem;
+		}
+		return n2;
+	}
+	static long gcdR(long n1, long n2) {
+		if(n1 == 0) return n2;
+		return gcdR(n2%n1, n1);
+	}
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
+		// BigInteger
 		BigInteger n1 = sc.nextBigInteger();
 		BigInteger n2 = sc.nextBigInteger();
-
 		BigInteger gcd = gcd(n1, n2);
-		
 		System.out.println(gcd);
+//		// Long
+//		long l1 = sc.nextLong();
+//		long l2 = sc.nextLong();
+//		long l3 = gcdI(l1, l2);
+//		long l4 = gcdR(l1,l2);
 		sc.close();
 	}
 }
