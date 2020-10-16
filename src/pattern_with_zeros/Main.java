601e63144f9737d9a895feed54d4c82bd458437d
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2020-10-17 00:03:37 +0530

    Fixed Pattern with Zeros Solution

--- src/pattern_with_zeros/Main.java
+++ src/pattern_with_zeros/Main.java
@@ -8,14 +8,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(sc.readLine().trim());
-        for(int row = 1; row <= n; row++){
+	static void patternWithZeros(int n) {
+		for(int row = 1; row <= n; row++){
             for(int col = 1; col <= row; col++)
                 if(col == 1 || col == row) System.out.print(row + "\t");
                 else System.out.print(0 + "\t");
             System.out.println();
         }
+	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine().trim());
+        patternWithZeros(n);
+        sc.close();
     }
 }
