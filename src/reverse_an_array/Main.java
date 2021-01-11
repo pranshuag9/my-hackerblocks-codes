6343fbb408967fa98c623d2a0ebf89ff3917981f
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-01-11 23:49:41 +0530

    Added Reverse an Array solution code using Java 8 Stream Class

---
+++ src/reverse_an_array/Main.java
@@ -0,0 +1,45 @@
+package reverse_an_array;
+/**
+ * @author Pranshu Aggarwal
+ * @problem https://hack.codingblocks.com/app/practice/2/1366/problem
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	static void reverseUsingFor(int[] arr) {
+		int head = 0, tail = arr.length - 1;
+		while(head < tail) {
+			swap(arr, head, tail);
+			head++;
+			tail--;
+		}
+	}
+	static void printArray(int[] arr) {
+		for(int i : arr) System.out.println(i);
+	}
+	public static void main(String[] args) throws Exception {
+		// Using this because of wrong input testcases in problem
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < arr.length; i++) arr[i] = sc.nextInt();
+//		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+//		int n = Integer.parseInt(sc.readLine().trim());
+//		long startTime, endTime;
+//		startTime = System.nanoTime();
+//		int[] arr = Arrays.stream(sc.readLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		reverseUsingFor(arr);
+		printArray(arr);
+//		endTime = System.nanoTime();
+//		System.out.println(endTime - startTime + "ns");
+		sc.close();
+	}
+}
