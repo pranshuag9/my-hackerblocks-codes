10103cd66cfdc9eea2924e89a244e5f9490d9c54
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-01-12 03:35:37 +0530

    Added Reverse an Array Solution code best optimized

--- src/reverse_an_array/Main.java
+++ src/reverse_an_array/Main.java
@@ -6,7 +6,6 @@ package reverse_an_array;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 	static void swap(int[] arr, int i, int j) {
@@ -17,24 +16,19 @@ public class Main {
 	static void reverseUsingFor(int[] arr) {
 		for(int head = 0, tail = arr.length - 1; head < tail; head++, tail--) swap(arr, head, tail);
 	}
-	static void printArray(int[] arr) {
-		for(int i : arr) System.out.println(i);
+	// Taking irregular testcases as input
+	static String takeInput(BufferedReader sc, int n) throws Exception {
+		String str = "";
+		while(str.split(" ").length <= n)
+			str += " " + sc.readLine().trim().replaceAll("\\s+", " ");
+		return str;
 	}
 	public static void main(String[] args) throws Exception {
-		// Using this because of wrong input testcases in problem
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr =	 new int[n];
-		for(int i = 0; i < arr.length; i++) arr[i] = sc.nextInt();
-//		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-//		int n = Integer.parseInt(sc.readLine().trim());
-//		long startTime, endTime;
-//		startTime = System.nanoTime();
-//		int[] arr = Arrays.stream(sc.readLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(sc.readLine().trim());
+		int[] arr = Arrays.stream(takeInput(sc, n).trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 		reverseUsingFor(arr);
-		printArray(arr);
-//		endTime = System.nanoTime();
-//		System.out.println(endTime - startTime + "ns");
+		for(int i : arr) System.out.println(i);
 		sc.close();
 	}
 }
--- src/reverse_an_array/main.py
+++ src/reverse_an_array/main.py
@@ -2,13 +2,24 @@
  @author: Pranshu Aggarwal
  @problem: https://hack.codingblocks.com/app/practice/2/1366/problem
 '''
-def reverseUsingFor(arr):
+import re
+def reverse_using_for(arr):
     for i, j in zip(range(0, len(arr), 1), range(len(arr) - 1, 0, -1)):
         if i < j: arr[i], arr[j] = arr[j], arr[i]
-def printArray(arr):
+def print_array(arr):
     for i in arr: print(i)
+# Taking irregular testcases like a Scanner Class in java
+def take_input(n):
+    arr = []
+    while True:
+        if len(arr) == n: break
+        line = input().strip()
+        re.sub(' +', ' ', line)
+        arr.extend(line.split())
+    return arr
 if __name__ == "__main__":
-    n = int(input())
-    arr = [int(i) for i in input().split()]
-    reverseUsingFor(arr)
-    printArray(arr)
+    n = int(input().strip())
+    arr = take_input(n)
+    arr = [int(i) for i in arr]
+    reverse_using_for(arr)
+    print_array(arr)
